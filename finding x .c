c4b929dce3833bbc8e5e277bd11b7a0b32864f9e
Shivrajjagtap9890 <jagtapshivraj84@gmail.com>
2026-09-25 22:57:15 +0530

    Add program to find x from a and b

---
+++ finding x .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    int a, b, x;
+
+    printf("Enter a: ");
+    scanf("%d", &a);
+
+    printf("Enter b: ");
+    scanf("%d", &b);
+
+    x = b - a;
+
+    printf("x = %d", x);
+}
